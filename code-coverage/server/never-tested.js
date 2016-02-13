0bea4ea9dba820168adc98abb5ed35db62df1eb4
sarkiroka <gabor.morvai@shiwaforce.com>
2016-02-13 22:55:55 +0100

    add some untested code

---
+++ code-coverage/server/never-tested.js
@@ -0,0 +1,8 @@
+module.exports = function (x) {
+	var retValue = null;
+	if (x) {
+		console.log(x);
+		retValue = x;
+	}
+	return retValue;
+};
--- code-coverage/server/tested-function.js
+++ code-coverage/server/tested-function.js
@@ -1,11 +1,13 @@
 var otherModule = require('./untested-function');
+var neverTestedModule = require('./never-tested');
 var debug = require('debug')('asd:f');
 module.exports = function (str) {
 	var retValue = null;
 	console.log('incoming parameter is "' + str + '", now otherModule source is', otherModule.toString());
 	if (('' + str).indexOf('server') == 0) {
 		console.log('normal run');
-		debug('normal run');
+		var x = neverTestedModule('test');
+		debug('normal run %s', x);
 		retValue = otherModule(1);
 	} else {
 		console.log('test case');
