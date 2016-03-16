0e07473bac5618c42843d680015bcc2d15857a4e
sarkiroka <sarki_roka@freemail.hu>
2016-03-16 23:24:36 +0100

    immediate

--- async/async/index.js
+++ async/async/index.js
@@ -5,20 +5,30 @@ var debug = require('debug')('sandbox:async:async');
 module.exports = function (initial, tasks, end) {
 	debug('run async');
 	for (var i = 0; i < tasks.length; i++) {
-		if (typeof tasks[i] == 'function') {
-			debug('run serial task');
-			tasks[i](initial, function () {
-			});
-		} else if (typeof tasks[i] == 'object' && typeof tasks[i].length == 'number') {
-			debug('run parallel tasks (%d)',tasks[i].length);
-			for (var j = 0; j < tasks[i].length; j++) {
-				debug('run parallel task');
-				tasks[i][j](initial, function () {
+		(function (tasksi) {
+			if (typeof tasksi == 'function') {
+				debug('run serial task');
+				setImmediate(function () {
+					tasksi(initial, function (err, params) {
+						debug('serial end %o', params);
+					});
 				});
+			} else if (typeof tasksi == 'object' && typeof tasksi.length == 'number') {
+				debug('run parallel tasks (%d)', tasksi.length);
+				for (var j = 0; j < tasksi.length; j++) {
+					(function (tasksij) {
+						debug('run parallel task');
+						setImmediate(function () {
+							tasksij(initial, function (err, params) {
+								debug('parallel end %o', params);
+							});
+						});
+					})(tasksi[j]);
+				}
+			} else {
+				debug('non runnable task');
 			}
-		}else{
-			debug('non runnable task');
-		}
+		})(tasks[i]);
 	}
-	end(null, initial)
+	end(null, initial);
 };
