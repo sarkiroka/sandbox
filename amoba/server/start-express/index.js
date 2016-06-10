c67ac0cff1c952ce832440e1e76e2afed2c5e297
sarkiroka <sarki_roka@freemail.hu>
2016-06-10 06:34:47 +0200

    v0.9.x setup express

--- amoba/server/start-express/index.js
+++ amoba/server/start-express/index.js
@@ -2,12 +2,10 @@
  * beállítja az express-t és elinditja a http kiszolgálást
  * @author sarkiroka on 2016.06.09.
  */
-var express = require('express');
 var setupExpress = require('./setup-express');
 var startServer = require('./start-server');
 module.exports = function () {
-	var app = express();
-	setupExpress(app, express);
+	var app = setupExpress(express);
 	var server = startServer(app);
 	return server;
 };
--- amoba/server/start-express/setup-express.js
+++ amoba/server/start-express/setup-express.js
@@ -2,9 +2,11 @@
  * az alkalmazásnak megfelelően beállítja a express libet
  * @author sarkiroka on 2016.06.10.
  */
+var express = require('express');
 var getClientPath = require('./get-client-path');
 var path = require('path');
 module.exports = function (app, express) {
+	var app = express();
 	app.set('view engine', 'pug');
 	var clientPath = getClientPath();
 	app.set('views', path.join(clientPath, 'views'));
