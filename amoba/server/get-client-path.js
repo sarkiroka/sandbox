a46ab12bccf2e5688091c48d6a43fca53a8007c3
sarkiroka <sarki_roka@freemail.hu>
2016-06-10 06:22:10 +0200

    v0.9.x client path

---
+++ amoba/server/get-client-path.js
@@ -0,0 +1,9 @@
+/**
+ * megadja a "client" mappa helyét
+ * @author sarkiroka on 2016.06.10.
+ */
+var path = require('path');
+module.exports = function () {
+	var retValue = path.normalize(path.join(__dirname, '..', 'client'));
+	return retValue;
+};
--- amoba/server/start-express.js
+++ amoba/server/start-express.js
@@ -2,14 +2,16 @@
  * beállítja az express-t és elinditja a http kiszolgálást
  * @author sarkiroka on 2016.06.09.
  */
+var getClientPath = require('./get-client-path');
 var express = require('express');
 var http = require('http');
 var path = require('path');
 module.exports = function () {
 	var app = express();
 	app.set('view engine', 'pug');
-	app.set('views', path.normalize(path.join(__dirname, '..', 'client', 'views')));
-	app.use('/static', express.static(path.normalize(path.join(__dirname, '..', 'client', 'static'))));
+	var clientPath = getClientPath();
+	app.set('views', path.join(clientPath, 'views'));
+	app.use('/static', express.static(path.join(clientPath, 'static')));
 	app.get('/', function (req, res) {
 		res.render('index')
 	});
