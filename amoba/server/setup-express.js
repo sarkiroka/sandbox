f787e756e06d30859121ba1c0c7e3f056d686c3d
sarkiroka <sarki_roka@freemail.hu>
2016-06-10 06:25:54 +0200

    v0.9.x setup express

---
+++ amoba/server/setup-express.js
@@ -0,0 +1,15 @@
+/**
+ * az alkalmazásnak megfelelően beállítja a express libet
+ * @author sarkiroka on 2016.06.10.
+ */
+var getClientPath = require('./get-client-path');
+var path = require('path');
+module.exports = function (app, express) {
+	app.set('view engine', 'pug');
+	var clientPath = getClientPath();
+	app.set('views', path.join(clientPath, 'views'));
+	app.use('/static', express.static(path.join(clientPath, 'static')));
+	app.get('/', function (req, res) {
+		res.render('index')
+	});
+};
--- amoba/server/start-express.js
+++ amoba/server/start-express.js
@@ -2,19 +2,12 @@
  * beállítja az express-t és elinditja a http kiszolgálást
  * @author sarkiroka on 2016.06.09.
  */
-var getClientPath = require('./get-client-path');
 var express = require('express');
 var http = require('http');
-var path = require('path');
+var setupExpress = require('./setup-express');
 module.exports = function () {
 	var app = express();
-	app.set('view engine', 'pug');
-	var clientPath = getClientPath();
-	app.set('views', path.join(clientPath, 'views'));
-	app.use('/static', express.static(path.join(clientPath, 'static')));
-	app.get('/', function (req, res) {
-		res.render('index')
-	});
+	setupExpress(app, express);
 	var httpServer = http.Server(app);
 	httpServer.listen(2700);
 	return httpServer;
